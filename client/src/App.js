d2fcf98300e9c0cad8f4290c67d21f860cc69dc2
Destiny Frith <desilyvahn@gmail.com>
2022-08-23 16:28:18 -0500

    setting up react router

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,26 @@
 import { useState, useEffect } from 'react';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useLocation,
+} from 'react-router-dom';
+
 import { accessToken, logout, getCurrentUserProfile } from './spotify'
 import { catchErrors } from './utils';
 import './App.css';
+
+// Scroll to top of page when changing routes
+// https://reactrouter.com/web/guides/scroll-restoration/scroll-to-top
+function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+}
 
 function App() {
   const [token, setToken] = useState(null);
@@ -26,19 +45,39 @@ function App() {
             Log in to Spotify
           </a>
         ) : (
-          <>
-            <button onClick={logout}>Log Out</button>
+          <Router>
+            <ScrollToTop />
 
-            {profile && (
-              <div>
-                <h1>{profile.display_name}</h1>
-                <p>{profile.followers.total} Followers</p>
-                {profile.images.length && profile.images[0].url && (
-                  <img src={profile.images[0].url} alt="Avatar" />
-                )}
-              </div>
-            )}
-          </>
+            <Routes>
+              <Route path="/top-artists">
+                Top Artists
+              </Route>
+              <Route path="/top-tracks">
+                Top Tracks
+              </Route>
+              <Route path="/playlists/:id">
+                Playlist
+              </Route>
+              <Route path="/playlists">
+                Playlists
+              </Route>
+              <Route path="/">
+                <>
+                  <button onClick={logout}>Log Out</button>
+
+                  {profile && (
+                    <div>
+                      {profile.display_name}
+                      <p>{profile.followers.total} Followers</p>
+                      {profile.images.length && profile.images[0].url && (
+                        <img src={profile.images[0].url} alt="Avatar" />
+                      )}
+                    </div>
+                  )}
+                </>
+              </Route>
+            </Routes>
+          </Router>
         )}
       </header>
     </div>
